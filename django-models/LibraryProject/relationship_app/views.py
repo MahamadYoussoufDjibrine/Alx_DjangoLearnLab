9da262a110ab0d6c9a347ff0661ecd094260888f
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-01-15 21:37:48 +0200

    Task 1: Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,14 +4,6 @@ from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library, Author
 
-# Function-based view to list all books
-def list_books(request):
-    """
-    Function-based view that displays all books in the database.
-    """
-    books = Book.objects.all().select_related('author')
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list, content_type='text/plain')
 
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
