2dfe2046988e31e61b4d8ea9daa4d1e63ecbaf30
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-01-15 07:44:52 +0200

    Task 1: add class-based LibraryDetailView and URL routing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,25 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Library, Book
+from .models import Book, Library
 
 
-# Function-based view (for checker completeness)
-def list_books(request, library_id):
-    library = get_object_or_404(Library, id=library_id)
-    books = library.books.all()
-    return render(request, "relationship_app/list_books.html", {
-        "library": library,
-        "books": books
-    })
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view (THIS is what the checker wants)
+
+# Class-based view: library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["books"] = self.object.books.all()
-        return context
