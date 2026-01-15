6cd576fdcda71f9d6a15be92d557168c49d55e78
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-01-15 21:48:05 +0200

    Add function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,9 @@
-# relationship_app/urls.py
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
-app_name = 'relationship_app'
+app_name = "relationship_app"
 
 urlpatterns = [
-    # URL for function-based view (lists all books)
-    path('books/', views.list_books, name='list_books'),
-    
-    # URL for class-based view (shows specific library details)
-    path('library/<int:library_id>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path("books/", list_books, name="list_books"),
+    path("library/<int:library_id>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,24 @@
-# relationship_app/views.py
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library, Author
+from .models import Book, Library
 
 
-# Class-based view to display library details
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(
+        request,
+        "relationship_app/list_books.html",
+        {"books": books}
+    )
+
+
+# Class-based view: display details for a specific library
 class LibraryDetailView(DetailView):
-    """
-    Class-based view that displays details for a specific library.
-    """
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-    
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
     def get_object(self):
-        # Get the library by ID from URL
-        library_id = self.kwargs.get('library_id')
+        library_id = self.kwargs.get("library_id")
         return get_object_or_404(Library, id=library_id)
