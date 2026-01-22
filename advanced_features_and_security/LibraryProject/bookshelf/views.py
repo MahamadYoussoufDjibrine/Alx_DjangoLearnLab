eee6a65c923edc936f0f24f127a14e9f93916533
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-01-22 22:08:08 +0200

    Task 1: Add book_list view and permissions documentation

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,21 +2,21 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 
 
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    return HttpResponse("List of books")
+
+
 @permission_required("bookshelf.can_create", raise_exception=True)
 def create_book(request):
-    return HttpResponse("Book creation allowed")
+    return HttpResponse("Create book")
 
 
 @permission_required("bookshelf.can_edit", raise_exception=True)
 def edit_book(request, book_id):
-    return HttpResponse("Book editing allowed")
+    return HttpResponse("Edit book")
 
 
 @permission_required("bookshelf.can_delete", raise_exception=True)
 def delete_book(request, book_id):
-    return HttpResponse("Book deletion allowed")
-
-
-@permission_required("bookshelf.can_view", raise_exception=True)
-def view_books(request):
-    return HttpResponse("Book viewing allowed")
+    return HttpResponse("Delete book")
