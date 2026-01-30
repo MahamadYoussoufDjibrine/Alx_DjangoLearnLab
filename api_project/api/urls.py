222b3501d9e7b85a1173f3cb3e43831025a505ae
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-01-30 20:11:30 +0200

    Task 3: Add token authentication with authtoken

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,9 +6,6 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Task 1 endpoint
     path('books/', BookList.as_view(), name='book-list'),
-
-    # Task 2 router endpoints
     path('', include(router.urls)),
 ]
