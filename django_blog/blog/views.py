b072777b16771149f48d18bc37c4153ee50c36d9
Mahamad Youssouf Djibrine <104102455+MahamadYoussoufDjibrine@users.noreply.github.com>
2026-02-14 14:37:39 +0200

    Task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm
 
 def home(request):
     return render(request, 'blog/home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created. You can now log in.")
+            return redirect("login")
+    else:
+        form = RegisterForm()
+    return render(request, "registration/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email", request.user.email)
+        request.user.save()
+        messages.success(request, "Profile updated.")
+        return redirect("profile")
+    return render(request, "registration/profile.html")
